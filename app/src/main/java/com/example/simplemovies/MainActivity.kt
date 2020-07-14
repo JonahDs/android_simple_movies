cfb04f6f4aed94d807fdd51f3674ddcc818dd68b
Jonah De Smet <jonah.desmet@student.hogent.be>
2020-07-14 17:25:24 +0200

    added navigation drawer

--- app/src/main/java/com/example/simplemovies/MainActivity.kt
+++ app/src/main/java/com/example/simplemovies/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.simplemovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import com.example.simplemovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
+
+        val navController = findNavController(R.id.navhostFragment)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.navhostFragment).navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
