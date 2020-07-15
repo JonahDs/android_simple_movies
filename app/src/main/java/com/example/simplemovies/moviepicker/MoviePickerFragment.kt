60d20f7f591c8763643166e3d7ebbc83278e38bc
Jonah De Smet <jonah.desmet@student.hogent.be>
2020-07-15 14:51:15 +0200

    added dropdown with custom adapter

--- app/src/main/java/com/example/simplemovies/moviepicker/MoviePickerFragment.kt
+++ app/src/main/java/com/example/simplemovies/moviepicker/MoviePickerFragment.kt
@@ -1,14 +1,24 @@
 package com.example.simplemovies.moviepicker
 
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModelProvider
+import com.example.simplemovies.MovieApplication
 import com.example.simplemovies.R
+import com.example.simplemovies.databinding.FragmentMoviePickerBinding
+import com.example.simplemovies.domain.GenresWrapper
 import javax.inject.Inject
 
 /**
@@ -16,16 +26,35 @@ import javax.inject.Inject
  */
 class MoviePickerFragment : Fragment() {
 
-    @Inject lateinit var viewmodelFactory: ViewModelProvider.Factory
+    @Inject
+    lateinit var viewmodelFactory: ViewModelProvider.Factory
 
     private val moviePickerViewModel by viewModels<MoviePickerViewModel> { viewmodelFactory }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity().application as MovieApplication).graph.moviepickerComponent().create().inject(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_picker, container, false)
+        val binding = FragmentMoviePickerBinding.inflate(inflater)
+        val text = binding.genresField
+
+        moviePickerViewModel.movieGenres.observe(viewLifecycleOwner, Observer {
+            val adapter = GenreAdapter(requireContext(), R.layout.list_item, it.genres)
+            (text.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+        })
+
+        (text.editText as? AutoCompleteTextView)?.setOnItemClickListener { parent, _, position, _ ->
+            Log.i("CLICKED", (parent.adapter as GenreAdapter).getItem(position)?.id.toString())
+            moviePickerViewModel.fetchMovieById((parent.adapter as GenreAdapter).getItem(position)?.id)
+        }
+
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/simplemovies/moviepicker/MoviePickerViewModel.kt
+++ app/src/main/java/com/example/simplemovies/moviepicker/MoviePickerViewModel.kt
@@ -1,9 +1,43 @@
 package com.example.simplemovies.moviepicker
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.example.simplemovies.domain.GenresWrapper
 import com.example.simplemovies.repositories.MovieRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MoviePickerViewModel @Inject constructor(private val movieRepo: MovieRepository) : ViewModel() {
 
+    private val viewModelJob = Job()
+
+    private val scope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    private var _movieGenres = MutableLiveData<GenresWrapper>()
+
+    val movieGenres : LiveData<GenresWrapper> get() = _movieGenres
+
+    init {
+        fetchGenres()
+    }
+
+    private fun fetchGenres() {
+        scope.launch {
+            try {
+                _movieGenres.value = movieRepo.getAllMovieGenres()
+            } catch (e: Exception) {
+                Log.i("GENRES_EX", e.message.toString())
+            }
+        }
+    }
+
+    fun fetchMovieById(genreId: Int?) {
+
+    }
 }
